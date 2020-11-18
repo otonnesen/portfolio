12cf33888dc3a235a93185992c1c0094f99f185a
Oliver Tonnesen <oliver@tonnesen.com>
2020-11-17 23:06:14 -0800

    Started rewrite.

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/otonnesen/portfolio
 
-go 1.13
+go 1.15
--- main.go
+++ main.go
@@ -1,84 +1,18 @@
 package main
 
 import (
-	"encoding/json"
-	"html/template"
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
-	"strings"
-
-	"github.com/otonnesen/portfolio/util"
 )
 
-var Projects []Project
-var ProjectDatabase map[string]Project
-var NotFound http.HandlerFunc
-
 func main() {
-	port := os.Getenv("PORT") // Get Heroku port
+	port := os.Getenv("PORT")
 
 	if port == "" {
 		log.Printf("$PORT not set, defaulting to 8080")
 		port = "8080"
 	}
 
-	// Create array of CSSData structs to pass to CompileCSS
-	styles := []string{
-		"home",
-		"projectpage",
-		"notfound",
-	}
-	err := util.CompileCSS(styles)
-	if err != nil {
-		log.Panicf("Error compiling CSS: %v\n", err)
-	}
-
-	ProjectDatabase = make(map[string]Project)
-
-	err = initProjects()
-	if err != nil {
-		log.Panicf("Error initializing projects: %v\n", err)
-	}
-
-	cwd, err := os.Getwd()
-	if err != nil {
-		log.Panicf("Error getting cwd: %v\n", err)
-	}
-	NotFound = util.ParseTemplate(NotFoundTemplate,
-		filepath.Join(cwd, "tmpl/notfound.tmpl"))
-	Root := util.ParseTemplate(RootTemplate,
-		filepath.Join(cwd, "tmpl/index.tmpl"))
-	ProjectPage := util.ParseTemplate(ProjectPageTemplate,
-		filepath.Join(cwd, "tmpl/projectpage.tmpl"))
-
-	http.Handle("/project/", util.LogRequest(ProjectPage))
-	http.HandleFunc("/", util.LogRequest(Root))
-	http.Handle("/static/", http.StripPrefix("/static/",
-		http.FileServer(http.Dir("static"))))
-	http.ListenAndServe(":"+port, nil)
-}
-
-func initProjects() error {
-	cwd, err := os.Getwd()
-	if err != nil {
-		return err
-	}
-	// Load JSON data
-	f, err := os.Open(filepath.Join(cwd, "data/projects.json"))
-	if err != nil {
-		return err
-	}
-
-	err = json.NewDecoder(f).Decode(&Projects)
-	if err != nil {
-		return err
-	}
-	f.Close()
-	for _, project := range Projects {
-		project.Body = template.HTML(strings.Join(project.BodyList, "\n"))
-		ProjectDatabase[project.Name] = project
-	}
-	return nil
+	log.Fatal(http.ListenAndServe(":"+port, http.FileServer(http.Dir("./static/"))))
 }
