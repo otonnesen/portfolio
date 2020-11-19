7ab13498e2d8cef7ef9744f010f89cdae090a6b3
Oliver Tonnesen <oliver@tonnesen.com>
2020-11-19 08:28:18 -0800

    Added logging.

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 )
 
+var logger *log.Logger
+
 func main() {
 	port := os.Getenv("PORT")
 
@@ -14,5 +16,24 @@ func main() {
 		port = "8080"
 	}
 
-	log.Fatal(http.ListenAndServe(":"+port, http.FileServer(http.Dir("./static/"))))
+	logFile, err := os.OpenFile("log.txt", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer logFile.Close()
+
+	logger = log.New(logFile, "", log.LstdFlags)
+
+	m := &middleware{http.FileServer(http.Dir("./static/"))}
+	log.Fatal(http.ListenAndServe(":"+port, m))
+}
+
+type middleware struct {
+	h http.Handler
+}
+
+func (m *middleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	logger.Printf("[%s]: %s %q", r.RemoteAddr, r.Method, r.URL.String())
+	m.h.ServeHTTP(w, r)
 }
